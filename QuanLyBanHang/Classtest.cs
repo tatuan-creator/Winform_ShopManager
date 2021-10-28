87077db0389e47289b64d116f8da28cf914f0c16
DDoo <kenlx8706@gmail.com>
2021-10-28 10:15:39 +0700

    add cs

---
+++ QuanLyBanHang/Classtest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyBanHang
+{
+    class Classtest
+    {
+        public string abc { get; set; }
+    }
+}
